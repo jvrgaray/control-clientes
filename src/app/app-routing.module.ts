b71cbdc1aac3ca5a62551535093e1f8a7578a06c
Javier Garay <javier.garay@2brains.cl>
2020-01-31 16:11:54 -0300

    73. Configuracion de rutas de la aplicacion de control de clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TableroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrarse', component: RegistroComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
+  { path: 'cliente/editar/:id', component: EditarClienteComponent },
+  { path: '**', component: NoEncontradoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
