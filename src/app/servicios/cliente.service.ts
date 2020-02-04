cd2653f9448799241c36f10eafc7687cc150602a
Javier Garay <javier.garay@2brains.cl>
2020-02-04 17:39:00 -0300

    78. Creacion del servicio de clientes

---
+++ src/app/servicios/cliente.service.ts
@@ -0,0 +1,31 @@
+import { Cliente } from './../modelo/cliente.model';
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ClienteServicio {
+  clientesColeccion: AngularFirestoreCollection<Cliente>;
+  clienteDoc: AngularFirestoreDocument<Cliente>;
+  clientes: Observable<Cliente[]>;
+  cliente: Observable<Cliente>;
+
+  constructor(private db: AngularFirestore) {
+    this.clientesColeccion = db.collection('clientes', ref => ref.orderBy('nombre', 'asc'));
+  }
+
+  getClientes(): Observable<Cliente[]> {
+    // Obtener los clientes
+    this.clientes = this.clientesColeccion.snapshotChanges().pipe(
+      map( cambios => {
+        return cambios.map( accion => {
+          const datos = accion.payload.doc.data() as Cliente;
+          datos.id = accion.payload.doc.id;
+          return datos;
+        });
+      })
+    );
+    return this.clientes;
+  }
+}
