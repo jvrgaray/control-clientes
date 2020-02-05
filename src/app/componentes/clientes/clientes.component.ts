551a207c5e67ffc500f174bfa61fb534ab13a005
Javier Garay <javier.garay@2brains.cl>
2020-02-05 16:52:25 -0300

    82. Agregar Cliente - parte 2

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { ClienteServicio } from './../../servicios/cliente.service';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/modelo/cliente.model';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-clientes',
@@ -10,8 +11,15 @@ import { Cliente } from 'src/app/modelo/cliente.model';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  };
 
-  constructor(private clientesServicio: ClienteServicio) { }
+  constructor(private clientesServicio: ClienteServicio,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
     this.clientesServicio.getClientes().subscribe(
@@ -25,15 +33,18 @@ export class ClientesComponent implements OnInit {
     let saldoTotal: number = 0;
 
     if (this.clientes != null) {
-      /*this.clientes.forEach(
-        cliente => {
-          saldoTotal += cliente.saldo;
-        }
-      );*/
-
       saldoTotal = this.clientes.reduce((total, actual) => total + actual.saldo, 0);
     }
     return saldoTotal;
   }
 
+  agregar({ value, valid }: { value: Cliente, valid: boolean }) {
+    if(!valid){
+      this.flashMessage.show('Por favor llena el formulario correctamente', {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    } else {
+      //agregar el nuevo cliente
+    }
+  }
 }
