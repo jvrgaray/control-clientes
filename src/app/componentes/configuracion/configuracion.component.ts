60a71653fd6a43733c3b5efd1117c645939ea436
Javier Garay <javier.garay@2brains.cl>
2020-02-11 17:43:58 -0300

    93. Componente configuracion - parte 1

--- src/app/componentes/configuracion/configuracion.component.ts
+++ src/app/componentes/configuracion/configuracion.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configuracion.component.css']
 })
 export class ConfiguracionComponent implements OnInit {
+  permitirRegistro = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  guardar() {
+
+  }
 }
