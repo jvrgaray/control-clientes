2593f67551696591a5968f189fd8705f0b212ab1
Javier Garay <javier.garay@2brains.cl>
2020-02-11 12:38:38 -0300

    85. Modificar cliente - parte 1

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/modelo/cliente.model';
+import { ClienteServicio } from 'src/app/servicios/cliente.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-cliente',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-cliente.component.css']
 })
 export class EditarClienteComponent implements OnInit {
+  clientes: Cliente[];
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  };
 
-  constructor() { }
+  id: string;
+
+  constructor(private clientesServicio: ClienteServicio,
+    private flashMessage: FlashMessagesService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+    this.clientesServicio.getCliente(this.id).subscribe(cliente => {
+      this.cliente = cliente;
+    });
   }
 
 }
